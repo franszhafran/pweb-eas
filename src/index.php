6f9f885b97540a5c83333a76d4a8b555df3cf362
billharit <billharit@gmail.com>
2021-12-24 01:56:38 +0700

    classview dah ada

--- src/index.php
+++ src/index.php
@@ -21,6 +21,7 @@ load_classphp('./');
 
 $route_map = new Kernel\RouteMap();
 $route_map->addRouteMap("/login", Controller\AdminController::class, "login");
+$route_map->addRouteMap("/classview", Controller\StudentController::class, "classview");
 $router = new Kernel\Router();
 $router->solve($_SERVER['REQUEST_URI'], $route_map);
 ?>
